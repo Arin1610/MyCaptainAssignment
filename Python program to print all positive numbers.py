0c47cf0d631365e534b412691420046ac046ecdd
Arin1610 <100861125+Arin1610@users.noreply.github.com>
2022-03-27 01:20:23 +0530

    Add files via upload

---
+++ Python program to print all positive numbers.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 27 01:08:39 2022
+
+@author: Arin
+"""
+# Python program to print positive Numbers in a List
+  
+list1 = [12, -7, 5, 64, -14]
+
+list2=[12,14,-95,3]
+  
+for num in list1:
+      
+    if num >= 0:
+       print(num, end = " ")
+       
+for num in list2:
+      
+    if num >= 0:
+       print(num, end = " " )
+       
