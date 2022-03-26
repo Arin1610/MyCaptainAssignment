11af57a41e5b6706dd8f910fcb39ba9cd43e8135
Arin1610 <100861125+Arin1610@users.noreply.github.com>
2022-03-27 01:23:13 +0530

    Add files via upload

---
+++ area_of_circle.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 16 11:31:52 2022
+
+@author: Arin
+"""
+
+#Accepts the radiius of circle from user and compute area.
+
+r=float(input("Enter Radius of the circle : "))
+area=22/7*r*r
+print("The area of the circle with radius ",r,"is : " ,area)
